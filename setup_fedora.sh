20f2f4cce5a8dbf9ed3dccde12c859f651a3ee23
Cameron Mann <cameron.mann@cybera.ca>
2015-10-07 16:45:31 -0600

    Add Fedora setup script

---
+++ setup_fedora.sh
@@ -0,0 +1,7 @@
+path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+sudo ln -s /usr/share/git-core/contrib/completion/git-prompt.sh /etc/profile.d
+
+sudo ln -s $path/fonts/conf.d/10-bci-hint.conf /etc/fonts/conf.d
+sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+sudo ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
