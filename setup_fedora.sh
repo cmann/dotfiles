f9212fc725709aad6493d08202fda1c7d4092e15
Cameron Mann <cameron.mann@cybera.ca>
2020-08-08 22:58:40 -0600

    Update fedora setup script

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -1,7 +1,18 @@
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-sudo ln -s /usr/share/git-core/contrib/completion/git-prompt.sh /etc/profile.d
+# mkdir ~/bin
 
-sudo ln -s $script_dir/fonts/conf.d/10-bci-hint.conf /etc/fonts/conf.d
-sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
-sudo ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf -y install docker-ce docker-ce-cli containerd.io neovim hub xclip
+python3 -m pip install --user --upgrade pynvim
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+
+curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o ~/bin/docker-compose
+curl -L "https://github.com/docker/machine/releases/download/v0.16.0/docker-machine-$(uname -s)-$(uname -m)" -o ~/bin/docker-machine
+
+# sudo ln -s /usr/share/git-core/contrib/completion/git-prompt.sh /etc/profile.d
+# sudo ln -s $script_dir/fonts/conf.d/10-bci-hint.conf /etc/fonts/conf.d
+# sudo ln -s /usr/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
+# sudo ln -s /usr/share/fontconfig/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
