7560d1b9b6bf5a7622d5ba670d9a913e8ec2edae
Cameron Mann <cameron.mann@cybera.ca>
2022-07-31 23:52:19 -0600

    emacs: update org-roam config to v2

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -145,16 +145,8 @@
 
 (use-package org-roam
   :delight
-  :hook (after-init . org-roam-setup)
-  :init (setq org-roam-v2-ack t)
-  :custom (org-roam-directory (file-truename "~/org/roam/"))
-  :bind (:map org-roam-mode-map
-              (("C-c n l" . org-roam)
-               ("C-c n f" . org-roam-find-file)
-               ("C-c n g" . org-roam-graph))
-              :map org-mode-map
-              (("C-c n i" . org-roam-insert))
-              (("C-c n I" . org-roam-insert-immediate))))
+  :config (org-roam-db-autosync-mode)
+  :custom (org-roam-directory (file-truename "~/org/roam/")))
 
 (use-package highlight-numbers
   :hook (prog-mode . highlight-numbers-mode)
